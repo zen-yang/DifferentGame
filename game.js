9ff4ddac353821f483c992b42a28140d0473dbc0
zen-yang <519796642@qq.com>
2020-12-06 22:58:49 +0800

    第八次提交 将不同模块拆分开，然后分别导入

---
+++ game.js
@@ -0,0 +1,48 @@
+var Game = function(fps) {
+  var g = {
+    actions: {},
+    keydowns: {},
+  }
+  var canvas = document.querySelector('#id-canvas')
+  var context = canvas.getContext('2d')
+  g.canvas = canvas
+  g.context = context
+  // draw
+  g.drawImage = function(gameImage) {
+    g.context.drawImage(gameImage.image, gameImage.x, gameImage.y)
+  }
+  // events
+  window.addEventListener('keydown', function(event){
+    g.keydowns[event.key] = true
+  })
+  window.addEventListener('keyup', function(event){
+    g.keydowns[event.key] = false
+  })
+  //
+  // 把按键 key 按下执行的函数存到 actions 里面
+  g.registerAction = function(key, callback) {
+    g.actions[key] = callback
+  }
+  // timer
+  setInterval(function(){
+    // events
+    // 遍历它的 key, 检查一下，如果被按下了，就执行函数
+    var actions = Object.keys(g.actions)
+    for (var i = 0; i < actions.length; i++){
+      var key = actions[i]
+      if (g.keydowns[key]) {
+        // 如果按键被按下，调用注册的 action
+        g.actions[key]()
+      }
+    }
+    // update
+    g.update()
+    // clear
+    context.clearRect(0, 0, canvas.width, canvas.height)
+
+    // draw
+    g.draw()
+  }, 1000/fps)
+
+  return g
+}
