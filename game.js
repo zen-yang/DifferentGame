3d592253cec6f014cd56e401285417928d3b9c09
zen-yang <519796642@qq.com>
2020-12-08 23:09:17 +0800

    第十次提交 重构创建计分板，重构部分代码

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
-var Game = function() {
+var Game = function(fps, images, runCallback) {
+  // images 是一个对象，里面是图片的引用名字和图片
+  // 程序会在所有图片载入成功后才运行
   var g = {
     actions: {},
     keydowns: {},
+    images: {},
   }
   var canvas = document.querySelector('#id-canvas')
   var context = canvas.getContext('2d')
@@ -49,9 +52,42 @@ var Game = function() {
     }, 1000/fps)
   }
 
-  setTimeout(function(){
-    runloop()
-  }, 1000/fps)
+  var loads = []
+  // 预先载入所有图片
+  var names = Object.keys(images)
+  for (var i = 0; i < names.length; i++) {
+    let name = names[i]
+    var path = images[name]
+    let img = new Image()
+    img.src = path
+    img.onload = function() {
+      // 存入 g.images 中
+      g.images[name] = img
+      // 所有图片都成功载入之后，调用 run
+      loads.push(1)
+      if (loads.length == names.length) {
+        g.run()
+      }
+    }
+  }
+
+  g.imageByName = function(name) {
+    var img = g.images[name]
+    var image = {
+      w: img.width,
+      h: img.height,
+      image: img,
+    }
+    return image
+  }
+
+  g.run = function() {
+    runCallback(g)
+    // 开始运行程序
+    setTimeout(function(){
+      runloop()
+    }, 1000/fps)
+  }
 
   return g
 }
--- paddle.js
+++ paddle.js
@@ -1,11 +1,15 @@
-var Paddle = function() {
-  var image = imageFromPath('paddle.png')
-  var o = {
-    image: image,
-    x: 100,
-    y: 250,
-    speed: 15,
-  }
+var Paddle = function(game) {
+  var o = game.imageByName('paddle')
+  // var o = {
+  //   image: image,
+  //   x: 100,
+  //   y: 250,
+  //   speed: 15,
+  // }
+  o.x = 100
+  o.y = 250
+  o.speed = 15
+  
   o.move = function(x) {
     if (x < 0) {
       x = 0
